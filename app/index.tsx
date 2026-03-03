4b41093504bcba3056a77be2006b4f328b5f3524
Erick Luna <up240801@alumnos.upa.edu.mx>
2026-03-03 15:33:38 -0600

    add getPokemon,fetch()

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
+import { use, useEffect } from "react";
 import { Text, View } from "react-native";
+import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
+
 
 export default function Index() {
+  useEffect(() => {
+    console.log("Enntre en pantalla");
+    getPokemon();
+  }, []  );
+
+  const getPokemon = async () => {
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log("Response: ", response);
+  };
+  
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>Erick</Text>
     </View>
   );
 }
