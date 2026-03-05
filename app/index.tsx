8a13f1b41df3ef83042fa08e1a790332e902c339
Erick Luna <up240801@alumnos.upa.edu.mx>
2026-03-04 20:02:22 -0600

    Show pokemon on screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
-import { use, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 
 
 export default function Index() {
+  const [results, setResult ] = useState<any[]>([]);
   useEffect(() => {
     console.log("Enntre en pantalla");
     getPokemon();
@@ -13,12 +14,16 @@ export default function Index() {
     const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
     const response = await fetch(URL);
     const data = await response.json();
-    console.log("Response: ", response);
+    console.log(data)
+    setResult(data.results);
   };
   
   return (
     <View>
-      <Text>Erick</Text>
+      <Text>{results[1]?.name}</Text>
+      {results.map((pokemon) =>{
+        return <Text key={pokemon.name}>{pokemon.name}</Text>
+      })}
     </View>
   );
 }
